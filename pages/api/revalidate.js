44e3b13e8b9014ba589c0bc21f978a7478e96921
Jean Pierre <jeanpierre.webedev@gmail.com>
2022-09-05 16:09:20 -0500

    revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  if (req.headers['Authorization'] === process.env.AUTHORIZATION) {
+  if (req.headers['authorization'] === process.env.AUTHORIZATION) {
     await res.unstable_revalidate('/')
     return res.json({ revalidate: true })
   }
