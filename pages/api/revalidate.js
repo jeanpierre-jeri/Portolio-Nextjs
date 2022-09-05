21cbcb29d1cf3cae8b050b3ea518e968d86b1c68
Jean Pierre <jeanpierre.webedev@gmail.com>
2022-09-05 16:27:42 -0500

    revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,8 +1,16 @@
 export default async function handler(req, res) {
-  if (req.headers['authorization'] === process.env.AUTHORIZATION) {
-    await res.unstable_revalidate('/')
-    return res.json({ revalidate: true })
+  if (req.headers['authorization'] !== process.env.AUTHORIZATION) {
+    return res.status(401).json({ message: 'Not Authorized' })
   }
 
-  return res.status(401).end()
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate('/')
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating')
+  }
 }
